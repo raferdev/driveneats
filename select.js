82a9d8df4a2b330b5c4833cda1ebfe87161a4f0a
Rafael <rafinha_fern@hotmail.com>
2022-01-31 03:56:48 -0300

    vs 2.5
    customizar pedido 50%

--- select.js
+++ select.js
@@ -1,4 +1,5 @@
-
+let total = 0
+let escolhas = 0
 function selecionar(tipo, produto, preco) {
    const selecionado = document.querySelector("." + tipo + ".border");
    if (selecionado !== null) {
@@ -8,12 +9,22 @@ function selecionar(tipo, produto, preco) {
          icone.classList.remove("aparecendo");
          icone.classList.add("some");
          selecionado.classList.remove("border");
+         if (escolhas == 3){
+            const fecharPedido = document.querySelector(".fechar_pedido");
+            fecharPedido.classList.remove("verde");
+            fecharPedido.innerHTML = "<p>Selecione os 3 itens</p><p>para fechar o pedido</p>";
+            fecharPedido.href.remove;
+            escolhas -= 1;
+            return;
+         }
+         escolhas -= 1;
          return;
       } else {
          const icone = document.querySelector(".ionicon" + "." + tipo + ".aparecendo");
          selecionado.classList.remove("border");
          icone.classList.remove("aparecendo");
          icone.classList.add("some");
+         escolhas -= 1;
       }
    }
    const click = document.querySelector("." + produto);
@@ -21,4 +32,11 @@ function selecionar(tipo, produto, preco) {
    click.classList.add("border");
    icone.classList.add("aparecendo");
    icone.classList.remove("some");
+   escolhas += 1;
+   if (escolhas == 3) {
+      const fecharPedido = document.querySelector(".fechar_pedido");
+      fecharPedido.classList.add("verde");
+      fecharPedido.innerHTML = "Fechar pedido";
+      fecharPedido.href.remove = "";
+   }
 }
