ef618e170495590037b818efdded1cb8ada54168
Rafael <rafinha_fern@hotmail.com>
2022-01-30 22:30:00 -0300

    vs 2.0
    selecionar via javascript

--- select.js
+++ select.js
@@ -1,10 +1,24 @@
- function selecionar (produto,preco) {
-     if (produto == "prato") {
-        const selecionado = document.querySelector("." + produto, ".border");
-        if (selecionado !== null) {
-            selecionado.classList.remove("border");
-            selecionado.classList.add("some")
-         }
-       } 
-    }
- 
+
+function selecionar(tipo, produto, preco) {
+   const selecionado = document.querySelector("." + tipo + ".border");
+   if (selecionado !== null) {
+      const repetiu = selecionado.classList.contains(produto);
+      if (repetiu) {
+         const icone = document.querySelector(".ionicon" + "." + tipo + ".aparecendo");
+         icone.classList.remove("aparecendo");
+         icone.classList.add("some");
+         selecionado.classList.remove("border");
+         return;
+      } else {
+         const icone = document.querySelector(".ionicon" + "." + tipo + ".aparecendo");
+         selecionado.classList.remove("border");
+         icone.classList.remove("aparecendo");
+         icone.classList.add("some");
+      }
+   }
+   const click = document.querySelector("." + produto);
+   const icone = document.querySelector(".ionicon" + "." + produto);
+   click.classList.add("border");
+   icone.classList.add("aparecendo");
+   icone.classList.remove("some");
+}
